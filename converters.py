e6d262addb58c77a780ff819aab595f62e4af0a7
Dmitry Lipnitskij <dmitrydust2704@gmail.com>
2022-10-24 18:34:35 +0300

    imge encoding/decoding

--- converters.py
+++ converters.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 
@@ -39,3 +40,52 @@ def decode_string(array):
             break
 
     return text
+
+
+def encode_image(image):
+    size = image.shape
+    array = []
+    size_arr = bin(size[0])[2:].zfill(12) + bin(size[1])[2:].zfill(12)
+    image = image / 255
+
+    for bit in size_arr:
+        array.append(np.uint8(bit))
+
+    for st in image:
+        for el in st:
+            array.append(el)
+
+    return array
+
+
+def decode_image(array):
+    size_string = ""
+    for i in range(24):
+        size_string += str(array[i])
+
+    size = (int("0b"+size_string[:12], 2), int("0b"+size_string[12:24], 2))
+    print(size)
+
+    image = np.zeros(size)
+
+    for i in range(size[0]):
+        for j in range(size[1]):
+            image[i,j] = array[j + i*size[1] + 24] * 255
+
+    return image
+
+
+if __name__ == "__main__":
+    test_im = np.zeros((300, 300))
+    test_im[20:40, 20:40] = 255
+
+    cv2.imshow("test_im", test_im)
+
+    array = encode_image(test_im)
+    rec_im = decode_image(array)
+
+    cv2.imshow("rec_im", rec_im)
+
+    print(np.all(test_im == rec_im))
+
+    cv2.waitKey(0)
